ebdf9d94fa6ecd3588e261e8b115718e437e9e09
inversia <mrs.shpritz@gmail.com>
2021-10-17 14:21:35 +0300

    NoteGenerator finished

--- src/pages/Main/PlanetaryMenu/NoteGenerator/NoteGenerator.tsx
+++ src/pages/Main/PlanetaryMenu/NoteGenerator/NoteGenerator.tsx
@@ -1,26 +1,81 @@
-import React, { useEffect, useState } from 'react'
+import React, { useEffect, useRef, useMemo } from 'react'
+import { throttle } from 'lodash'
 import './NoteGenerator.scss'
+import { rescale } from '~common'
+import _notesImages from './icons/*.svg'
+
+const notesImages = Object.values(_notesImages as { [key: string]: string })
 
 export function NoteGenerator() {
-    const [isOnNoteArea, setIsOnNoteArea] = useState(false)
+    // const [isOnNoteArea, setIsOnNoteArea] = useState(false)
 
-    // const mouseDetecting: React.MouseEventHandler = e => {
-
-    //     window.addEventListener('mousemove', {(e) => console.log(e.clientX)})
-    // }
-
-    const [position, setPosition] = useState<{ x: number; y: number }>()
+    const position = useRef({ x: 0, y: 0 })
+    const notesArea = useRef<HTMLDivElement>(null)
 
     useEffect(() => {
-        const setMousePosition: React.MouseEventHandler = e => {
-            setPosition({ x: e.clientX, y: e.clientY })
-            window.removeEventListener('mousemove', setMousePosition)
-        }
+        const setMousePosition = (e: MouseEvent) =>
+            (position.current = { x: e.clientX, y: e.clientY })
 
         window.addEventListener('mousemove', setMousePosition)
-        console.log('position', position)
-    }, [position])
 
-    // return <div className="note-gen-wrapper" onMouseMove={e => mouseDetecting(e)}></div>
-    return <div className="note-gen-wrapper"></div>
+        return () => document.removeEventListener('mousemove', setMousePosition)
+    }, [])
+
+    const trottledCreateNotes = useMemo(
+        () =>
+            throttle(() => {
+                const { left, top, width, height } = (
+                    notesArea.current as HTMLElement
+                )?.getBoundingClientRect()
+
+                if (notesArea.current && position) {
+                    const el = document.createElement('DIV')
+                    el.classList.add('note')
+                    notesArea.current.appendChild(el)
+
+                    el.style.setProperty(
+                        '--note-size',
+                        `${Math.ceil(rescale(Math.random(), [0, 1], [1, 5]))}vw`
+                    )
+                    const noteImageIndex = Math.round(
+                        rescale(Math.random(), [0, 1], [0, notesImages.length])
+                    )
+                    if (notesImages)
+                        el.style.setProperty('--note-image', `url(${notesImages[noteImageIndex]})`)
+
+                    el.style.setProperty(
+                        '--note-opacity',
+                        `${rescale(Math.random(), [0, 1], [0.3, 1])}`
+                    )
+
+                    const noteLeft = position.current.x - left
+                    const noteTop = position.current.y - top
+                    el.style.transform = `translate(${noteLeft}px, ${noteTop}px)`
+
+                    setTimeout(() => {
+                        el.remove()
+                    }, 950)
+                }
+            }, 100),
+
+        []
+    )
+
+    console.log(notesImages[0])
+    console.log(Math.round(rescale(Math.random(), [0, 1], [0, notesImages.length])))
+
+    return (
+        <div className="note-gen-wrapper">
+            <div
+                className="notes-container"
+                ref={notesArea}
+                onMouseMove={trottledCreateNotes}
+                style={
+                    {
+                        '--note-size': `${Math.ceil(rescale(Math.random(), [0, 1], [1, 10]))}px`
+                    } as React.CSSProperties
+                }
+            ></div>
+        </div>
+    )
 }
