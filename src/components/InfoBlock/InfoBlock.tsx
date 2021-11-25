b5416439834e2ac850e27633b18066622e82af41
inversia <mrs.shpritz@gmail.com>
2021-11-25 06:35:18 +0300

    favicon add

--- src/components/InfoBlock/InfoBlock.tsx
+++ src/components/InfoBlock/InfoBlock.tsx
@@ -21,30 +21,29 @@ export function InfoBlock({
     picturesArray?: string[]
 }) {
     return (
-        <VisibilitySensor scrollCheck={true} scrollThrottle={100} partialVisibility={true}>
+        <VisibilitySensor scrollCheck={true} scrollThrottle={300} partialVisibility={true}>
             {({ isVisible }) => (
                 <div
                     className={cls('info-block-wrapper', {
                         'text-right': textAtRight,
-                        vertical: isVertical
+                        vertical: isVertical,
+                        visible: isVisible
                     })}
                 >
                     <div className="info-wrapper">
                         <div className="info">{text}</div>
                     </div>
-                    {!picturesArray && image && isVisible ? (
+                    {!picturesArray && image ? (
                         <div
                             className="illustration"
                             style={{ backgroundImage: `url(${image})` }}
                         ></div>
                     ) : (
-                        isVisible && (
-                            <Image
-                                index={index || 0}
-                                picturesArray={picturesArray || []}
-                                className={'illustration'}
-                            />
-                        )
+                        <Image
+                            index={index || 0}
+                            picturesArray={picturesArray || []}
+                            className={'illustration'}
+                        />
                     )}
                 </div>
             )}
--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import ReactDom from 'react-dom'
 import panic from 'panic-overlay'
-panic.configure({ projectRoot: '/Users/new/if8' })
+panic.configure({ projectRoot: '/Users/new/vityaz' })
 
 // import './fonts.css'
 import './reset.css'
